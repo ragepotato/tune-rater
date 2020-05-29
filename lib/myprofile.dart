397b9e202e05219842dcd5ca5e0197ac924162b0
Stephen Tayag <stephen.taya@gmail.com>
2020-05-28 22:41:28 -0700

    Working on profile pic.

--- lib/myprofile.dart
+++ lib/myprofile.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:crypto/crypto.dart';
 
-
+import 'dart:convert';
 
 
 class MyProfilePage extends StatefulWidget {
@@ -20,6 +21,7 @@ class _MyProfilePageState extends State<MyProfilePage> {
   var passController = TextEditingController();
   String signinError = "";
   var currentUser = "Unknown";
+  String gravatarPic = "";
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final DatabaseReference dataRef = FirebaseDatabase.instance.reference();
@@ -34,6 +36,33 @@ class _MyProfilePageState extends State<MyProfilePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text("Username: " + widget.username.toString()),
+            RaisedButton(
+              child: Text("Picture"),
+              onPressed: (){
+                gravatarPic = md5.convert(utf8.encode("Stephen.tayag@gmail.com ".trim().toLowerCase())).toString();
+                setState(() {
+                  gravatarPic = "https://www.gravatar.com/avatar/" + gravatarPic + "?s=500";
+                  print(gravatarPic);
+                });
+
+
+
+              },
+            ),
+            IconButton(
+              icon: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    gravatarPic),
+                radius: 80,
+
+              ),
+              onPressed: (){
+                print("Expand icon");
+              },
+              iconSize: 160,
+
+            ),
+
           ],
         ),
       ),
